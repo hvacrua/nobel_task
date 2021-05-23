cb73586bc252615b5242e4b0f312892ad27755ab
hvacrua <kras_pp@ukr.net>
2021-05-23 14:40:28 +0300

    final_version

--- export_to_google_sheet.py
+++ export_to_google_sheet.py
@@ -47,15 +47,69 @@ def Create_Service(client_secret_file, api_service_name, api_version, *scopes):
 Create_Service('client_secret_375823794938-voiuv7v2fd8af6gij7d8cmci5bekq6ga.apps.googleusercontent.com.json', 'sheets', 'v4', ['https://www.googleapis.com/auth/spreadsheets'])
 
 
-def Export_Data_To_Sheets():
+def export_data_to_sheets():
     response_date = service.spreadsheets().values().update(
         spreadsheetId = SAMPLE_SPREADSHEET_ID_input,
         valueInputOption = 'RAW',
         range = SAMPLE_RANGE_NAME,
-        body=dict(majorDimension = 'ROWS',
+        body = dict(majorDimension = 'ROWS',
                   values = df.T.reset_index().T.values.tolist())
     ).execute()
     print('Sheet successfully Updated')
 
 
-Export_Data_To_Sheets()
+def set_sheet_format():
+    """Set format for sheet"""
+    sheetId = 0
+    results = service.spreadsheets().batchUpdate(
+        spreadsheetId = SAMPLE_SPREADSHEET_ID_input,
+        body = {"requests": [
+            {"repeatCell": {"cell": {"userEnteredFormat":
+                {"horizontalAlignment": 'CENTER',
+                 "backgroundColor": {"red": 0.8, "green": 0.8, "blue": 0.8, "alpha": 1},
+                 "textFormat": {"bold": True, "fontSize": 12}}
+                                     },
+                             "range": {"sheetId": sheetId,
+                                       "startRowIndex": 0,
+                                       "endRowIndex": 1,
+                                       "startColumnIndex": 0,
+                                       "endColumnIndex": df.shape[1]},
+                             "fields": "userEnteredFormat"
+                            }
+            },
+            {'updateBorders': {'range': {'sheetId': sheetId,
+                                         'startRowIndex': 0,
+                                         'endRowIndex': df.shape[0] + 1,
+                                         'startColumnIndex': 0,
+                                         'endColumnIndex': df.shape[1]},
+                               # set style for bottom border
+                               'bottom': {'style': 'SOLID',
+                                          'width': 1,
+                                          'color': {'red': 0, 'green': 0, 'blue': 0, 'alpha': 1}},
+                               # set style for top border
+                               'top': {'style': 'SOLID',
+                                       'width': 1,
+                                       'color': {'red': 0, 'green': 0, 'blue': 0, 'alpha': 1}},
+                               # set style for left border
+                               'left': {'style': 'SOLID',
+                                        'width': 1,
+                                        'color': {'red': 0, 'green': 0, 'blue': 0, 'alpha': 1}},
+                               # set style for right border
+                               'right': {'style': 'SOLID',
+                                         'width': 1,
+                                         'color': {'red': 0, 'green': 0, 'blue': 0, 'alpha': 1}},
+                               # set style for internal horizontal lines
+                               'innerHorizontal': {'style': 'SOLID',
+                                                   'width': 1,
+                                                   'color': {'red': 0, 'green': 0, 'blue': 0, 'alpha': 1}},
+                               # set style for internal vertical lines
+                               'innerVertical': {'style': 'SOLID',
+                                                 'width': 1,
+                                                 'color': {'red': 0, 'green': 0, 'blue': 0, 'alpha': 1}}
+                               }}
+        ]
+        }).execute()
+
+
+export_data_to_sheets()
+set_sheet_format()
